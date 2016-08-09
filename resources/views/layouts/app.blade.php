a51ffae2ee84b8bc76ce71ef1969fd0ef9ca7dc6
Scrivs <scrivs@gmail.com>
2016-08-09 10:59:32 -0700

    init

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,16 @@
             $(document).foundation();
         </script>
         @if (ENV('APP_ENV') == 'production' && Auth::guest())
-            
+            <script>
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+              ga('create', 'UA-51364-66', 'auto');
+              ga('send', 'pageview');
+
+            </script>
         @endif
     </body>
 </html>
