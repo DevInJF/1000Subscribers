bb7348c3b41a76703d2098883b487c76efbc7ed6
Scrivs <scrivs@gmail.com>
2016-08-10 19:55:29 -0700

    init

--- resources/views/challenge/day1.blade.php
+++ resources/views/challenge/day1.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.app', ['title' => 'Optimizing Your Homepage - 1000 Subscribers'])
 
 @section('content')
 <section class="hero">
@@ -10,16 +10,68 @@
         </div>
     </div>
 </section>
-<section class="content">
+<section class="content article">
 	<div class="row">
 		<div class="small-12 columns">
-			<h2>Time to get started</h2>
+			<p>Here is the scenario.</p>
+			<p>You have a big meeting with a famous actor. What&rsquo;s this meeting about? It doesn&rsquo;t matter, just know that it is big.</p>
+			<p>The goal is to get the famous actor to donate to your world famous charity.</p>
+			<p>Yes, you are big time.</p>
+			<p>So how do you go about ensuring that you give yourself the best chance of getting that donation?</p>
+			<ol>
+				<li>Do you ask them over and order takeout?</li>
+				<li>Do you take them out to an extravagant dinner?</li>
+				<li>Do you show them the purpose of your charity and the good that it does?</li>
+			</ol>
+			<p>It really depends on the actor&rsquo;s personality, but most of you probably went with #3 and it&rsquo;s a good choice.</p>
+			<p>Why? Because it sets the stage for things to come and gets the person in the right mindset.</p>
+			<p>This is what you blog&rsquo;s homepage does.</p>
+			<h2>Your #1 Page</h2>
+			<p>When taking a look at your analytics you will always find that your homepage is one of your most popular pages. It makes sense because people like to click on that big logo at the top of the page to get taken back to the beginning.</p>
+			<p>Because your homepage is so popular, it is one of the best opportunities you have to gain subscribers and yet so many people let it go to waste.</p>
+			<p>Instead of treating your homepage like a landing page, you probably treat it like a blog listing page. The logic being that when people come to your homepage they want to see the rest of your blog posts and you are right, they do.</p>
+			<p>But they can see blog posts on almost every other page on your site. Let your homepage take on a different job.</p>
+			<p>When people visit your homepage, this is your chance to let them know what your site is about to also entice them with an offer they don&rsquo;t want to refuse.</p>
+			<p>Instead of talking about yourself, talk about the benefits that a person will get from being on your site.</p>
+			<p>I call this your <em>promise to your audience</em>.</p>
+			<h2>The Hero Section</h2>
+			<p>Let&rsquo;s take a look at how some popular bloggers handle their promises to their audiences.</p>
+			<h3><a href="http://byregina.com">By Regina</a></h3>
+			<div class="image"><a href="http://byregina.com"><img src="https://s3-us-west-2.amazonaws.com/obstacleco-assets/wp-content/uploads/2016/07/03232028/byregina.com_.jpg"></a></div>
+			<h3><a href="http://www.chrisducker.com/">Chris Ducker</a></h3>
+			<div class="image"><a href="http://chrisducker.com"><img src="https://s3-us-west-2.amazonaws.com/obstacleco-assets/wp-content/uploads/2016/07/03234131/chrisducker.com_.jpg"></a></div>
+			<h3><a href="http://www.femtrepreneur.co/">Femtrepreneur</a></h3>
+			<div class="image"><a href="http://www.femtrepreneur.co/"><img src="https://s3-us-west-2.amazonaws.com/obstacleco-assets/wp-content/uploads/2016/07/03234742/femtrepreneur.co_.jpg"></a></div>
+			<p>Notice that every single one is trying to grab a subscriber right off the bat. The hero section, that&rsquo;s the giant section you see at the top of a lot sites, is the one section that doesn&rsquo;t get ignored by readers.</p>
+			<p>It&rsquo;s the perfect opportunity to put your arm around your reader&rsquo;s shoulder and make them an offer. All they have to do is give you their email.</p>
+			<p>What is even better about this technique is that you don&rsquo;t have to resort to pop-ups.</p>
+			<p>I understand why people like to use pop-ups, they are super effective, but they are also super annoying.</p>
+			<p><strong>You can get the same kind of attention using your hero section effectively as you could by placing a giant, content-blocking pop-up on your page.</strong></p>
+			<h2>The Paths</h2>
+			<p>What happens after someone goes past your hero section? Is that where you provide them with a lovely list of your blog posts?</p>
+			<p>You can, but there is a better use of this real estate.</p>
+			<p>Odds are that you have a couple of different actions that you want someone to take when they visit your site.</p>
+			<p>Maybe you have 3 separate email sequences that people can join. Maybe you are a freelancer and you want people to contact you about your services. Maybe you have a YouTube channel that you would love for people to check out.</p>
+			<p>What a lot of people will do is put this kind of stuff in the sidebar, but the sidebar is largely ignored. The web has been around for over 20 years and sidebars were there on day 1 so your audience has grown accustomed to not paying attention to them.</p>
+			<p>Instead you can give them another section that lays out some clear paths that you want them to follow.</p>
+			<div class="image"><a href="http://melyssagriffin.com"><img src="https://s3-us-west-2.amazonaws.com/obstacleco-assets/wp-content/uploads/2016/08/11025047/melyssa_griffin_paths.png"></a></div>
+			<p>Here is the section <a href="http://melyssagriffin.com">Melyssa Griffin</a> has under her hero section.</p>
+			<p>As you can see she has done a nice job of laying out 3 clear paths for her audience to follow:</p>
+			<ol>
+				<li>Sign up for a free workshop</li>
+				<li>View her e-courses</li>
+				<li>Join her Facebook community</li>
+			</ol>
+			<p>These are all paths that help her business out, while also providing some type of value to the reader.</p>
+			<p>Look at the logic of having a section like this right after the hero. A person see the hero offer, but decides it might not be for them or maybe they want to browse more. Right away they are presented with 3 more offers (paths) that they can make a decision on.</p>
+			<p>Overall, you are showing 4 different options to your reader to get them to take a specific action and none of this looks sleazy in any way.</p>
 		</div>
 	</div>
 </section>
 <section class="content toc">
 	<div class="row">
 		<div class="small-12 columns">
+			<h2>The Schedule</h2>
 			<ul class="no-bullet days">
 				<li class="active">Day 1: <span>Optimizing Your Homepage</span></li>
 				<li class="passive">Day 2: <span>Reverse Engineering Great Content</span></li>
