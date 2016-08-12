1d3421504d4bd56bc69f7394e2cdd3d402c29b8b
Scrivs <scrivs@gmail.com>
2016-08-11 20:33:13 -0700

    init

--- resources/views/challenge/day2.blade.php
+++ resources/views/challenge/day2.blade.php
@@ -4,16 +4,134 @@
 <section class="hero">
     <div class="row">
         <div class="small-12 columns">
-        	<h3 class="subheader">List-Building Challenge Day 1:</h3>
-            <h1>Examining What Makes Content Great</h1>
+        	<h3 class="subheader">List-Building Challenge Day 2:</h3>
+            <h1>What Makes Great Content Great?</h1>
             <h2 class="subheader">Hacking the Secrets of Great Content</h2>
         </div>
     </div>
 </section>
-<section class="content">
+<section class="content article">
 	<div class="row">
 		<div class="small-12 columns">
-			<h2>Time to get started</h2>
+			<div class="callout">
+				<p>Did you end up here because someone was kind enough to link this page? That&rsquo;s cool, but you&rsquo;ll want to sign up yourself so you can experience all 7 days of this list-building challenge.</p>
+				<p><a data-open="emailModal" class="button expanded">Start Your Free 7-Day Challenge</a></p>
+			</div>
+			<p><em>Content is king.</em></p>
+			<p>If you haven&rsquo;t heard this already then be prepared because as you continue to read more and more blogs you will see this phrase all over the place.</p>
+			<p>The story behind this phrase is that in the early days of the web you could get away with any type of content simply because there wasn&rsquo;t much competition.</p>
+			<p>If you wanted to talk about kite-building, you could become the world&rsquo;s most popular kite-building blogger overnight.</p>
+			<p>As more and more blogs started to appear the chances of just winging became less and less.</p>
+			<p>People started to have more options when it came to looking for particular knowledge. If your content sucked then they could just move on to the next thing.</p>
+			<p>Hence, <em>content is king</em>.</p>
+			<h2>Great Content Builds Connections</h2>
+			<p>Think of all of the pieces of content you&rsquo;ve linked to on your social media platform of choice. Why did you link it?</p>
+			<p>If you are already running a blog think about the other blogs that you link to. Why do you link to them?</p>
+			<p>You link to them because you feel that your audience will find them valuable.</p>
+			<p>That is what great content does for you. People want to share great content. You don&rsquo;t want to share mediocre content because what you share is a reflection of you.</p>
+			<p>Your audience only grows when others send people your way. They aren&rsquo;t going to send their audience to pieces of content that aren&rsquo;t worth their audience&rsquo;s time.</p>
+			<p>When the influencers in your industry notice that you&rsquo;ve written great post after great post they will reach out to you and look to see how you can partner up.</p>
+			<p>Great content builds connections.</p>
+			<p>Alright, you understand the value of content so that&rsquo;s a good thing. You now know that you need to create great content, but what is that?</p>
+			<h2>Your Time Is Valuable</h2>
+			<p>You&rsquo;re taking time out of your day to read this post. If you finish reading it and consider it a giant waste of time, then is that great content to you?</p>
+			<p>Not at all.</p>
+			<p>The measure of whether or not content is great is decided by the person reading it, not by the person writing it. However, that doesn&rsquo;t mean you can&rsquo;t figure out what the majority of your audience considers to be great content.</p>
+			<p>All you need to do is some hacking.</p>
+			<h2>Hacking Great Content</h2>
+			<p>If you&rsquo;ve ever seen a comedian go to work you would think that most of what they do is natural talent. That is how they start off, but the great comedians understand the structure of a good joke.</p>
+			<p>Same goes for great writers. They know how to construct a story in a way that pulls their audience in.</p>
+			<p>Now nobody is expecting you to start winning Pulitzer Prizes with your blog posts, but they do expect that you are going to bring something of value into their lives.</p>
+			<h3>Be Useful</h3>
+			<p>Every day people are looking for ways to improve their lives. It could be finding a new healthy recipe or maybe it&rsquo;s a better understanding of how SEO works.</p>
+			<p>When they read a blog post they want to leave with something.</p>
+			<p>Every blog post you write needs to be extremely useful. If it isn&rsquo;t then you have just wasted the reader&rsquo;s time.</p>
+			<p>This means that you need to consider the reader. Remember, it is about them, not you.</p>
+			<p>Everything about the post should center around the reader. What are their needs, wants, hopes, and dreams? What problems do they have in their daily lives that you can solve?</p>
+			<p>Now you choose a topic that will solve one of their problems. Help them achieve something that they&rsquo;ve always wanted to achieve.</p>
+			<h3>Plan It Out</h3>
+			<p>I have two methods to writing content:</p>
+			<ol>
+				<li>Blindly writing about an idea that I have</li>
+				<li>Researching a topic and planning out the blog post</li>
+			</ol>
+			<p>#1 happens a lot, but the funny thing is, I end up going back and conducting research and re-arranging my blog post. #1 doesn&rsquo;t save me any time because I just end up doing all the work that I do with #2.</p>
+			<p>Great blog posts follow a structure. Your reader will know if you put some time and effort into writing it. They appreciate effort and usually reward it with their time.</p>
+			<p>Take the time to plan out your blog post. Map out the problem you are looking to solve and the solutions you will present.</p>
+			<p>What points do you want to address? What questions will you answer?</p>
+			<p>Writing an outline for your posts will allow you to see which points you want to cover and the best order in which to cover them in.</p>
+			<h3>Headlines Are the Neon Signs</h3>
+			<p>Great headlines pull people in. Mediocre headlines make people think twice about clicking the link.</p>
+			<p>Sites like <a href="http://buzzfeed.com">Buzzfeed</a> have mastered the clickbait headlines. You don&rsquo;t have to go to the crazy extremes that they do, but you can learn a thing or two about how they approach headline creation.</p>
+			<p>A great headline clarifies exactly what your blog post is about.</p>
+			<p>You should try your best to write your headline first so you understand what the main point of your post will be. It doesn&rsquo;t need to be the final headline, but it has to be something that keeps you focused.</p>
+			<p>The few words that you do use in your post title will be the most important words you ever use for a blog post.</p>
+			<p>Why?</p>
+			<p>Well, think about it. How do people get to your site? It&rsquo;s usually through Google or some social media platform. They don&rsquo;t see the full blog post. All they see is the post title. That is where they make their decision on whether or not they are going to invest their time with you.</p>
+			<p>If the headline is catchy, they&rsquo;ll click over to read more. If it&rsquo;s not then you&rsquo;ve lost a reader before they even stepped through the door.</p>
+			<p>So how do you write great headlines?</p>
+			<p>The American Writers &amp; Artists teach the Four U&rsquo;s approach to writing awesome headlines.</p>
+			<ol>
+				<li>Be USEFUL to the reader</li>
+				<li>Provide them with a sense of URGENCY</li>
+				<li>Convey the idea that the main benefit is somehow UNIQUE</li>
+				<li>Do all of the above in an ULTRA-SPECIFIC way</li>
+			</ol>
+			<p>That is a good start, but we want a little bit more to work with. Copywriter Clayton Makepeaces believes that you should ask yourself six questions before you start to write your headline:</p>
+			<ol>
+				<li>Does your headline offer the reader a reward for reading?</li>
+				<li>What specifics could you add to make your headline more intriguing and believable?</li>
+				<li>Does your headline trigger a strong, actionable emotion the reader already has about the subject at hand?</li>
+				<li>Does your headline present a proposition that will instantly get your prospect nodding his or her head?</li>
+				<li>Could your headline benefit from the inclusion of a proposed transaction?</li>
+				<li>Could you add an element of intrigue to drive the prospect into your opening copy?</li>
+			</ol>
+			<p>Okay, now we are onto something. Makepeace&rsquo;s six questions combined with the basic structure of the Four U&rsquo;s provide us with an awesome framework to writing headlines that get clicked.</p>
+			<p>It will take you a bit of practice to really start to nail headline creation, but you&rsquo;ll find that the practice pays off significantly with the number of clicks you are able to generate.</p>
+			<h3>Nail the Intro</h3>
+			<p>You can usually tell how good a movie is going to be by the opening scene. Same thing applies to a blog post.</p>
+			<p>A boring introduction makes you space out, while an intriguing intro makes you pull your face closer to the screen.</p>
+			<p>Every great blog post grabs the reader&rsquo;s attention. If you lose them in the first few paragraphs then you&rsquo;ve basically lost them forever.</p>
+			<p>To pull them in you can tell a story or a joke, be empathetic, or use a fascinating fact or statistic.</p>
+			<p>Once you do that you can move on to the purpose of the post and explain how it will address the problem the reader is having. This ensures that the reader has a reason to continue reading.</p>
+			<p>Here is an example of a great intro to a blog post:</p>
+			<blockquote>
+				<p>Know what&rsquo;s tougher than creating great content?</p>
+				<p>Creating it at scale.</p>
+				<p>But that&rsquo;s exactly what&rsquo;s needed if you&rsquo;re serious about building a popular blog.</p>
+				<p>And almost every successful blogger reaches a point where they realize they can&rsquo;t do it alone.</p>
+			</blockquote>
+			<h3>Make the Post Scannable</h3>
+			<p>Yesterday, we talked about <a href="/challenge/optimizing-your-homepage">making your homepage scannable</a> and the same thing applies to your content.</p>
+			<p>Nobody wants to come across a huge wall of text. It&rsquo;s intimidating and can make someone believe they have better ways to spend their time.</p>
+			<p>It&rsquo;s hard for your reader to make a decision on whether or not a post is going to be worth their time if they can&rsquo;t easily digest it to take some key points from it.</p>
+			<p>Don&rsquo;t make your reader burrow through paragraphs to try and decipher what the post is about. They won&rsquo;t do it. They have better things to do.</p>
+			<p>Making your post scannable means that your reader can quickly glance through the post and pick up the main points. These main points will dictate whether or not they stick around.</p>
+			<p>The best ways to break up content to make it scannable are:</p>
+			<ul>
+				<li>lists</li>
+				<li>subheadings</li>
+				<li>block quotes</li>
+				<li>images</li>
+				<li>bold and italic text</li>
+			</ul>
+			<p>Place these things strategically throughout your post and you&rsquo;ve made things much easier for your readers.</p>
+			<h3>Be Human</h3>
+			<p>Finding your voice can be one of the most difficult things to do as a blogger. When I first started to blog I wanted to sound super professional as if I was writing for the <em>New York Times</em>.</p>
+			<p>It never worked because I don&rsquo;t sound like and my audience didn&rsquo;t want to read stuff like that.</p>
+			<p>You&rsquo;re writing blog posts, not dissertations. So when you write make sure it is in a plain, concise, and common-sense style.</p>
+			<p>Your reader wants to go back and read your content because it is great, not because they had trouble understanding it.</p>
+			<p>Keep it simple.</p>
+			<p>People like to read writing that is conversational. Write in a way that makes it seem like you are speaking to them at a bar, not in a classroom.</p>
+			<h2>Write, Write, Write</h2>
+			<p>Writing great content doesn&rsquo;t happen overnight. Like any other skill it is something that you have to continue to work on, but over time you will see some great improvement.</p>
+			<p>Because the focal point of your blog is your content, you need to ensure that you take the time to continually work on it.</p>
+			<p>Your content pulls the audience in and helps to convince them that it is worth handing over their email. It makes them believe that you have more value to offer them.</p>
+			<p>If they don&rsquo;t believe that then they will simply leave.</p>
+			<div class="callout">
+				<p>Did you end up here because someone was kind enough to link this page? That&rsquo;s cool, but you&rsquo;ll want to sign up yourself so you can experience all 7 days of this list-building challenge.</p>
+				<p><a data-open="emailModal" class="button expanded">Start Your Free 7-Day Challenge</a></p>
+			</div>
 		</div>
 	</div>
 </section>
@@ -32,4 +150,5 @@
 		</div>
 	</div>
 </section>
+@include('modal')
 @endsection
