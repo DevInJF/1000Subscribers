1c17c904224cc93821c327d0fd180c85b93a3016
Scrivs <scrivs@gmail.com>
2016-08-15 10:07:17 -0700

    init

--- resources/views/challenge/day3.blade.php
+++ resources/views/challenge/day3.blade.php
@@ -10,7 +10,7 @@
         </div>
     </div>
 </section>
-<section class="content">
+<section class="content article">
 	<div class="row">
 		<div class="small-12 columns">
 			<h2>Location, Location, Location</h2>
@@ -19,7 +19,65 @@
 			<p>The reason why is because the success of your store is dependent on foot traffic. If you are in a location that nobody frequents then you will have a tough time bringing people in.</p>
 			<p>However, if you&rsquo;re a bar that opens across the street from the new football stadium then things are going to be looking up for you.</p>
 			<p>What does your blog have to do with real estate?</p>
-			<p>Think of the biggest blog in your niche. That is the football stadium. If you could grab the attention of the big blog&rsquo;s audience to alert them about your blog, you might be onto something.</p>
+			<p>Think of the biggest blog in your niche. That is the football stadium. <strong>If you could grab the attention of the big blog&rsquo;s audience to alert them about your blog, you might be onto something.</strong></p>
+			<p>This is where guest posting comes into play.</p>
+			<h2>About Guest Posting</h2>
+			<p>So what is guest posting?</p>
+			<p>Guest posting is when you write content for another blog. It has been around since dinosaurs.</p>
+			<p>The arrangement usually works like this:</p>
+			<ol>
+				<li>You hit up a blog and pitch them a guest post</li>
+				<li>They accept your post</li>
+				<li>They publish it</li>
+				<li>People click the link in your author bio which is usually below the post</li>
+			</ol>
+			<p>As you can see you don&rsquo;t get paid for guest-posting. <strong>The value is in the link back to your site.</strong> This gives you a chance to get your site in front of a whole new audience that might not come across your site otherwise.</p>
+			<h3>The Perils of Guest Posting</h3>
+			<p>As of right now this sounds like roses.</p>
+			<p>You write something and then it miracuously shows up on another person&rsquo;s site and you get all of their traffic?</p>
+			<p>Not quite.</p>
+			<p>There are some potential downfalls to guest posting.</p>
+			<h4>1. Rejection</h4>
+			<p>When you are just starting off and you haven&rsquo;t built up any relationships yet, you need to spend time pitching your guest posts. This obviously takes a little bit of time and it&rsquo;s quite possible your time will be wasted.</p>
+			<p>Just because a site accepts guest posts that doesn&rsquo;t mean it will accept your guest post.</p>
+			<p>However, you do not know this unless you try.</p>
+			<h4>2. Lack of Traffic</h4>
+			<p>As mentioned, the idea is to send traffic back to your site. However, there is no guarantee that the readers will click on the link in your bio at the bottom. They could very easily just bypass.</p>
+			<p>Again, you won&rsquo;t know how successful this will be until you either talk to someone who has guest posted on the site or get a post up yourself.</p>
+			<h4>3. It Takes a Lot of Time</h4>
+			<p>You want to get the best of the best content up on other sites to show your value. Writing awesome posts take time and it can be frustrating to write something super nice only to see it bomb.</p>
+			<p>Sometimes it works out and sometimes it doesn&rsquo;t.</p>
+			<h2>How to Guest Post</h2>
+			<p>Some blogs will straight up tell you if they accept guest posts or not. Others might not be so obvious.</p>
+			<p>Let&rsquo;s pretend you&rsquo;ve identified a site that has the audience you are looking to reach. How do you get a guest post up on their site?</p>
+			<p><strong>The best way is to first establish a relationship with the blog owner.</strong> Tweet our their links, leave comments, and be helpful in general. You don&rsquo;t have to do this for very long. The idea is that they will recognize your name when you shoot them an email which will make them open it.</p>
+			<p>Within that email you should let them know that you would love the opportunity to write a post for their site. Then you pitch them 3 different ideas.</p>
+			<p>The key here is knowing what their audience wants. If you haven&rsquo;t gone through the blog&rsquo;s archives to see what type of content they produce, then press the brakes and go do that now.</p>
+			<p>The idea is that you want to know more about the blog than the owner does so you know exactly what type of content their audience is looking for.</p>
+			<p><strong>Warning:</strong> Check to make sure that the blog has author profiles at the end of their guest posts. If you can&rsquo;t get a link back to your site then there really is no point in writing for the blog unless we are talking <em>The New York Times</em>.</p>
+			<h2>Getting Subscribers</h2>
+			<p>Okay, so now you have a guest post about to be published to an awesome blog. How do you ensure you get subscribers?</p>
+			<p>Almost the exact same way you do when you post on your own site.</p>
+			<p>You need to offer the audience a content upgrade (we cover this tomorrow) that pertains to your guest post.</p>
+			<p>From there you want to link to a landing page that is all about that content upgrade and the messaging is meant for that blog&rsquo;s audience only. You want them to feel like the offer is only exclusive to them.</p>
+			<p>For example, if I write a guest post on how to gain 10 subscribers in the next hour, I wouldn&rsquo;t link to the <a href="/7-day-challenge">List-Building Challenge</a> landing page. Instead, I would create a custom page (same design, different messaging) that welcomed readers from that particular blog.</p>
+			<h2>Syndication: The Alternative to Guest Posting</h2>
+			<p>There is an alternative to guest posting that you might want to consider as well.</p>
+			<p>Syndication.</p>
+			<p>Syndication is a great option if you feel like guest posting might be a waste of your time and you want all of your best content on your own site.</p>
+			<p>With syndication you publish a post on your site and then reach out to other sites that might want to republish it.</p>
+			<p>You don&rsquo;t have to worry about any type of Google penalty because Google understands the original source of the content.</p>
+			<p>Because the post is already written you don&rsquo;t need to spend as much time trying to establish a relationship. You can simply reach out to the editor and explain that you have a post that you feel would benefit their audience.</p>
+			<p>Just like guest posting you want to ensure that you get a quality link back to your site.</p>
+			<p>If you choose to go the syndication route then you will want to look at the bigger blogs run by companies since they are more open to such things.</p>
+			<p>I suggest you give both guest posting and syndication a shot to see which one works best for you.</p>
+			<h2>Today&rsquo;s Tasks</h2>
+			<ul>
+				<li>Gather a list of blogs that are bigger than yours and have the same audience you are trying to attract.</li>
+				<li>Take a look at their most popular posts to get an idea of what their audience likes.</li>
+				<li>Check to see if they have a page dedicated to guest-posting. If not see what social media platforms the person hangs out at and try to establish a relationship. This may take longer than one day.</li>
+				<li>Think of 3 blog post titles that you could write for them and those will be in your pitch.</li>
+			</ul>
 		</div>
 	</div>
 </section>
