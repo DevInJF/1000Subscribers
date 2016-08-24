813e04f930c5891073536454d788283dc20b6a59
Scrivs <scrivs@gmail.com>
2016-08-24 08:26:44 -0700

    init

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -6,7 +6,7 @@
 		<div class="small-12 columns">
 			<h1 class="sans-serif">Gain 1,000 Subscribers.<br>Make $1,000.</h1>
 			<h2 class="subheader">Do both in 90 days.</h2>
-			<p><a href="" class="button large">I&rsquo;m ready to grow my audience and revenue</a></p>
+			<p><a href="http://learn.obstacle.co/courses/gain-1000#block-1148694" class="button large">You know what? I&rsquo;m ready to grow my audience and revenue! <i class="fa fa-arrow-right"></i></a></p>
 		</div>
 	</div>
 </section>
@@ -42,14 +42,24 @@
 		</div>
 	</div>
 </section>
+<section class="content oil">
+	<div class="row">
+		<div class="small-12 columns">
+			<h2 class="text-center">Is it possible to make <em>$3,000 over a weekend</em> from a list of <em>50 people</em>? Yep. I&rsquo;ve done it. Twice.</h2>
+			<h3 class="text-center">Trust me, you can do it too.</h3>
+		</div>
+	</div>
+</section>
 <section class="content gray">
 	<div class="row">
 		<div class="small-12 columns">
-			<h2 class="text-center">Wondering how you make job-quitting money with your blog?</h2>
+			<h2 class="text-center">Wondering how you can make job-quitting money with your blog?</h2>
 			<p>You see someone else making a healthy income from blogging and you figure you will give it a shot. However, you discover that it isn&rsquo;t that easy.</p>
 			<p>It seems like every blogger you read is making good money for you.</p>
 			<p>You&rsquo;ve tried ads and sponsored posts, but with so little traffic the most you make is $13 a month.</p>
 			<p>You definitely aren&rsquo;t going to keep a roof over your head with that much.</p>
+			<p>Some bloggers wait a year before they make their first $100! That&rsquo;s 9 months too long.</p>
+			<p><strong>90 days is all you need.</strong> In most cases, you don&rsquo;t even need that long.</p>
 		</div>
 	</div>
 </section>
@@ -69,24 +79,24 @@
 <section class="content oil">
 	<div class="row">
 		<div class="small-12 columns">
-			<h2 class="text-center">What if you had a perfect 90-day gameplan for building your audience and making money with your blog?</h2>
+			<h2 class="text-center">What if you had a perfect 90-day gameplan for building your audience and making money with your blog? What would you gain?</h2>
 		</div>
 		<div class="small-12 large-4 columns">
 			<div class="benefits-box">
-				<h3 class="text-center">Tactics</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos cum ullam temporibus necessitatibus, est perspiciatis! Voluptatibus soluta atque nemo, odit animi magni. Cupiditate, nulla unde voluptatibus delectus voluptatum, consequuntur et.</p>
+				<h3 class="text-center">Freedom</h3>
+				<p>Knowing exactly what you need to do provides you with a bit more freedom. It means you don&rsquo;t have to spend weeks or months trying to figure out how to do things the right way. You just do them.</p>
 			</div>
 		</div>
 		<div class="small-12 large-4 columns">
 			<div class="benefits-box">
-				<h3 class="text-center">Tactics</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos cum ullam temporibus necessitatibus, est perspiciatis! Voluptatibus soluta atque nemo, odit animi magni. Cupiditate, nulla unde voluptatibus delectus voluptatum, consequuntur et.</p>
+				<h3 class="text-center">Happiness</h3>
+				<p>You know what happens when you start to see positive results? You become a happier person. Imagine waking up and seeing emails from people that have brought your products and services.</p>
 			</div>
 		</div>
 		<div class="small-12 large-4 columns">
 			<div class="benefits-box">
-				<h3 class="text-center">Tactics</h3>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos cum ullam temporibus necessitatibus, est perspiciatis! Voluptatibus soluta atque nemo, odit animi magni. Cupiditate, nulla unde voluptatibus delectus voluptatum, consequuntur et.</p>
+				<h3 class="text-center">Value</h3>
+				<p>One of the best things in the world is knowing that you are helping others. Providing value in another person&rsquo;s life is extremly rewarding. That is why it is so awesome building a tribe that loves the value you bring.</p>
 			</div>
 		</div>
 	</div>
@@ -94,8 +104,171 @@
 <section class="content">
 	<div class="row">
 		<div class="small-12 columns">
-			<h3 class="text-center">Here is the solution you&rsquo;ve been looking for</h3>
-			<div class="text-center"><h2 class="text-center sans-serif jumbo yellow">The Earn 1,000 Course</h2></div>
+			<div class="callout double">
+				<h3 class="text-center">Here is the solution you&rsquo;ve been looking for</h3>
+				<div class="text-center"><h2 class="text-center sans-serif jumbo yellow">The Gain 1,000 Course</h2></div>
+			</div>
+			<h2 class="text-center">Opens September 1st</h2>
+		</div>
+		<div class="small-12 large-6 columns">
+			<h3>The Curriculum</h3>
+			<table>
+				<tbody>
+					<tr class="heading">
+						<td>Module 0: Welcome</td>
+					</tr>
+					<tr>
+						<td>Why this course was created</td>
+					</tr>
+					<tr>
+						<td>How this course works</td>
+					</tr>
+					<tr>
+						<td>About your instructor</td>
+					</tr>
+					<tr class="heading">
+						<td>Module 1: Why email?</td>
+					</tr>
+					<tr>
+						<td>The power of familiarity</td>
+					</tr>
+					<tr>
+						<td>How people make purchasing decisions</td>
+					</tr>
+					<tr class="heading">
+						<td>Module 2: About your tribe</td>
+					</tr>
+					<tr>
+						<td>What is a tribe?</td>
+					</tr>
+					<tr>
+						<td>Your why and your message</td>
+					</tr>
+					<tr>
+						<td>Your promise to your Tribe</td>
+					</tr>
+					<tr>
+						<td>Finding the <em>Restless Pathfinder</em></td>
+					</tr>
+					<tr>
+						<td>Raising your flag</td>
+					</tr>
+					<tr class="heading">
+						<td>Module 3: Setting yourself up for success</td>
+					</tr>
+					<tr>
+						<td>Tools required to build an email list</td>
+					</tr>
+					<tr>
+						<td>Setting your site up for success</td>
+					</tr>
+					<tr>
+						<td>Using powerful landing pages</td>
+					</tr>
+					<tr>
+						<td>Tracking your conversion rates with Google Analytics</td>
+					</tr>
+					<tr class="heading">
+						<td>Module 4: Growing your mailing list</td>
+					</tr>
+					<tr>
+						<td>Using content upgrades</td>
+					</tr>
+					<tr>
+						<td>Creating awesome email courses</td>
+					</tr>
+					<tr>
+						<td>Why everyone likes giveaways</td>
+					</tr>
+					<tr>
+						<td>Why pop-ups work</td>
+					</tr>
+					<tr class="heading">
+						<td>Module 5: Getting people to your site</td>
+					</tr>
+					<tr>
+						<td>Why you should syndicate your content</td>
+					</tr>
+					<tr>
+						<td>How to guest post on other blogs</td>
+					</tr>
+					<tr>
+						<td>Using Pinterest to explode your traffic</td>
+					</tr>
+					<tr>
+						<td>Webinars are the new spiderwebs</td>
+					</tr>
+					<tr>
+						<td>Amping up your social profiles</td>
+					</tr>
+					<tr class="heading">
+						<td>Module 6: Keeping your audience around</td>
+					</tr>
+					<tr>
+						<td>Setting up autoresponders and email sequences</td>
+					</tr>
+					<tr>
+						<td>How to push out continuous value</td>
+					</tr>
+					<tr class="heading">
+						<td>Module 7: Making your first $1,000</td>
+					</tr>
+					<tr>
+						<td>Money-making options for your blog</td>
+					</tr>
+					<tr>
+						<td>The quickest thing to sell to make your first $1,000</td>
+					</tr>
+					<tr>
+						<td>Using joint ventures to speed things up</td>
+					</tr>
+					<tr>
+						<td>Creating tripwires for quick-hit money</td>
+					</tr>
+					<tr class="heading">
+						<td>Bonus lessons</td>
+					</tr>
+					<tr>
+						<td>Setting up a sales funnel with ConvertKit</td>
+					</tr>
+					<tr>
+						<td>The 5 types of content to write when starting off</td>
+					</tr>
+					<tr>
+						<td>How to get 100 Twitter followers every day for free</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<div class="small-12 large-6 columns">
+			<h3>What&rsquo;s Inside</h3>
+			<p>This is an online-based video course where you will get to see me walk through the different steps necessary to get to 1,000 subscribers and $1,000.</p>
+			<h4>30 Rocking Videos</h4>
+			<p>The curriculum is easily laid out in a way that makes it a piece of cake to follow. Each video is awesome.</p>
+			<h4>10 Killer Online Worksheets</h4>
+			<p>You don&rsquo;t get to sit back and just watch videos.</p>
+			<p>Not here.</p>
+			<p>Instead, you get to actively do your homework while watching videos.</p>
+			<p>Time to go back to school, but fortunately for you, this school will help you make money without drowning you in loans.</p>
+			<p>Stupid 12th Century Cat Art Degree.</p>
+			<h4>An Awesome Online Community</h4>
+			<p>There is no reason to feel alone when taking on this venture. You will gain access to the online Slack community where you can interact with others taking the same journey as you.</p>
+			<p>Have a question? We are there to help answer it.</p>
+			<div class="callout">
+				<h3>90-Day Money Back Guarantee</h3>
+				<p>I promise that if after 90 days you haven&rsquo;t made your first $1,000 then you can get your money back.</p>
+				<p>If you follow the course and do what is necessary there is no reason why you shouldn&rsquo;t succeed so I am laying it all out there. <strong>Make your money or get your money back.</strong></p>
+				<p>You just need to promise me that you will put the work in.</p>
+			</div>
+			<h3>Unlimited Access Forever</h3>
+			<p>Things change over time.</p>
+			<p>New and better techniques to acquire subscribers and make money appear every year. One of the worst things you can do to yourself is stagnant.</p>
+			<p>What you learned yesterday might not be relevant tomorrow.</p>
+			<p><strong>That is why <em>The Gain 1,000</em> course will always be evolving.</strong></p>
+			<p>That also means you will always have access to it.</p>
+			<p>When new versions launch, you&rsquo;ll have access.</p>
+			<p>When new features are added, you&rsquo;ll have access.</p>
+			<p><strong>Pay once, learn forever.</strong></p>
 		</div>
 	</div>
 </section>
@@ -105,20 +278,64 @@
 			<h2 class="text-center">The Origin Story</h2>
 		</div>
 		<div class="small-12 large-6 columns">
-			<p>Everybody has a dream lifestyle they want to live. Sometimes that involves traveling, sometimes that means spending more time with family, and sometimes it means flying to Mars. Either way, we each have a lifestyle that we want and therefore we are searching for ways to attain it.</p>
-			<p>Obstacle.co was created to show you how you can achieve that lifestyle through blogs and online sites that amplify your passions.</p>
-			<h3>Erm, Blogs?</h3>
-			<p>Blogs?!</p>
-			<p>Yes, blogs. Obstacle.co chronicles my journey of using blogs as a way to get towards my dream lifestyle. </p>
-			<p>Why blogs? Well, you&#8217;re probably thinking of blogs with pictures of cats or gossip celebrity and while those are blogs, I&#8217;m talking about the type that offer value to people. Blogs allow your voice to get out there and are an easy way to provide awesome value to your tribe.</p>
-			<h3>A Different Approach to Teaching</h3>
-			<p>Obstacle.co is different from the other sites that teach you how to make money online. Most sites use only one site as an example of how to succeed and it&#8217;s always the site you are reading. That works well because then the site just continues to grow, but it always seems to be a site about how to make money.</p>
-			<p>So is the only way you can make money online by creating a <em>how to make money online site</em>? Of course not, and that is what separates Obstacle.co.</p>
-			<p>I&#8217;ll talk about the principles that I use on this site and the other blogs that I will be creating. Some of these blogs will be worked on solely by me, while others will be collaborations.</p>
+			<p>90 minutes.</p>
+			<p>That&rsquo;s how long it took me to get to work in the morning. I don&rsquo;t even want to tell you how long it took to get home.</p>
+			<p>I never figured I was meant for this type of life. I enjoyed life too much to waste most of it sitting at a desk making someone else rich.</p>
+			<p>Don&rsquo;t get me wrong. I loved the people I worked with and did a lot of amazing things, but those 49 little voices in my head were always telling me that I should aim for something more.</p>
+			<p>So I figured I was going to sell something online. I was going to write a book!</p>
+			<p>I sold 0 copies.</p>
+			<p>I was determined to make it on my own so I tried another book.</p>
+			<p>I made $32.</p>
+			<p>Sigh.</p>
+			<p>I had to be doing something wrong because everyone else I saw online was making a killing with their products.</p>
+			<p>Then I discovered the <em>secret</em>.</p>
+			<p>They had an audience.</p>
+			<p>More importantly, they had a mailing list of people waiting to buy from them.</p>
 		</div>
 		<div class="small-12 large-6 columns">
-			<div class="image"><img data-pin-nopin="true" src="https://s3-us-west-2.amazonaws.com/obstacleco-assets/wp-content/uploads/2016/06/22144311/treeme.jpg" alt="Paul Scrivens"></div>
+			<div class="image" style="margin-bottom: 5px;"><img data-pin-nopin="true" src="https://s3-us-west-2.amazonaws.com/obstacleco-assets/wp-content/uploads/2016/06/22144311/treeme.jpg" alt="Paul Scrivens"></div>
 			<p class="caption">This is me. Big nostrils, gap in teeth, losing hair and an inability to button shirts all the way.</p>
+		</div>
+	</div>
+</section>
+<section class="content oil">
+	<div class="row">
+		<div class="small-12 columns">
+			<h2 class="text-center">I&rsquo;ve <em>made over $160,000 in the past 18 months</em> thanks to my ability to build mailing lists.</h2>
+			<h3 class="text-center">They are the difference between living the dream and simply dreaming about it.</h3>
+		</div>
+	</div>
+</section>
+<section class="content">
+	<div class="row">
+		<div class="small-12 large-6 columns">
+			<div class="image" style="margin-bottom: 5px;">
+				<img style="margin-bottom: 0;" src="https://s3-us-west-2.amazonaws.com/obstacleco-assets/wp-content/uploads/2016/08/24150308/mcmoney.jpg">
+			</div>
+			<p class="caption">This is how it should be.</p>
+		</div>
+		<div class="small-12 large-6 columns">
+			<h2>Why I Created this Course</h2>
+			<p>My name is Paul Scrivens and I&rsquo;m that goofy looking guy above.</p>
+			<p>I created this course because of my parents.</p>
+			<p>I have spent my whole life watching them work at jobs they didn&rsquo;t enjoy just so they could provide for my family.</p>
+			<p>I created this course because of my friends.</p>
+			<p>Every year they tell me stories about looking for something new.</p>
+			<h3>I Created This Course for You</h3>
+			<p>It&rsquo;s no fun trying to figure out how to grow your blog and make money. In fact, it can be infuriating watching others around you pull in money when you know they aren&rsquo;t smarter or more talented.</p>
+			<p>You put in just as much work as anyone else and yet you aren&rsquo;t seeing the same results.</p>
+			<p>That sucks.</p>
+			<p>I have been in that boat before and I wanted to get off as quickly as possible.</p>
+			<p>This course was created so you don&rsquo;t have to go through the years of struggle that I did. It was created to provide you with a clear roadmap to success.</p>
+			<p>Are you ready to follow it?</p>
+		</div>
+	</div>
+</section>
+<section class="hero">
+	<div class="row">
+		<div class="small-12 columns">
+			<h2>It&rsquo;s Go Time!</h2>
+			<p><a href="http://learn.obstacle.co/courses/gain-1000#block-1148694" class="button large">2 affordable payment options. Let&rsquo;s go! <i class="fa fa-arrow-right"></i></a></p>
 		</div>
 	</div>
 </section>
