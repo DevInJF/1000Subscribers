e73ecf01109dee59a38c3263d5fdebdb2682aa0a
Scrivs <scrivs@gmail.com>
2016-08-31 17:40:24 -0700

    init

--- resources/views/challenge/day6.blade.php
+++ resources/views/challenge/day6.blade.php
@@ -23,14 +23,50 @@
 			<p>However, you can&rsquo;t just put together any type of random giveaway. You have to put together something that appeals to your audience.</p>
 			<p>How do you do that?</p>
 			<p>Context.</p>
-			<h2>Again, it&rsquo;s About Context</h2>
+			<h2>Again, It&rsquo;s About Context</h2>
 			<p>Let&rsquo;s pretend you run a dog training blog. Good for you!</p>
 			<p>You decide you want to do a giveaway where the prize is an iPad. That is a pretty damn good prize so you should expect a ton of people to sign up to win.</p>
 			<p>In fact, a ton of people do sign up!</p>
 			<p>There is just one problem.</p>
 			<p>Most of these people don&rsquo;t care about anything else you do. When you send out your first non-iPad related email over 50% of them unsubscribe. They were there for the iPad, not dog training tips.</p>
 			<p>This is where a lot of giveaways fail.</p>
-			<p>Sure you want to give offer great prizes, but you want to make sure you are bringing in the right audience.</p>
+			<p>Sure you want to give offer great prizes, but you want to make sure you are bringing in the right audience. Your prizes should directly relate to the topic of your blog.</p>
+			<p>For Obstacle.co, it would make sense to offer a year subscription to Convertkit and other things that relate to making money with your blog.</p>
+			<p>Anything outside of that will be a waste of time.</p>
+			<p>The goal is to grow your list, but you want to grow it with the right type of people.</p>
+			<p>You won&rsquo;t make your first $1,000 by sending out offers to random people. You need to have a laser-focused list that is ready to buy your products and services.</p>
+			<h2>How Giveaways Go Viral</h2>
+			<p>To be successful with a giveaway it is important that it reaches as many people as possible. This sounds obvious, but there are a ton of giveaways that aren&rsquo;t setup for success.</p>
+			<p>Think about the usual giveaway.</p>
+			<ol>
+				<li>You sign up.</li>
+			</ol>
+			<p>That&rsquo;s it!</p>
+			<p>That is the one magical step for most giveaways. How is that going to go viral?</p>
+			<p>Instead, you need to add in another step:</p>
+			<ol>
+				<li>You sign up.</li>
+				<li>You share the giveaway with everyone you know.</li>
+			</ol>
+			<p>Now your giveaway is starting to reach more people. However, you need to offer some type of incentive if you want people to share your giveaway because otherwise it doesn't make sense for them to lower their odss.</p>
+			<p>So what can you do? You can give them more entries into the contest when they share.</p>
+			<p>If they share on Twitter they get another entry. If they share on Facebook they get another entry.</p>
+			<p>This helps to quickly spread your contest to the largest possible audience.</p>
+			<h2>Planning Out Your Giveaway</h2>
+			<p>If you&rsquo;ve decided to do a giveaway you don&rsquo;t want to simply create one and be done with it. You need a marketing plan just like you do with every other awesome thing on your site.</p>
+			<p>Even though you are hoping for your giveaway to go viral that doesn&rsquo;t mean you can get away with very little promotion.</p>
+			<p>You don&rsquo;t want to miss a single person in your audience so let your mailing list, Twitter, Facebook, Instagram, Periscope, Vine, and wherever else your audience is know about the giveaway.</p>
+			<p>Let them know today.</p>
+			<p>Let them know tomorrow.</p>
+			<p>Let them know next week.</p>
+			<p>A viral giveaway only works when you have people signing up for it so don&rsquo;t be shy about promoting it.</p>
+			<h2>KingSumo</h2>
+			<p>To run a giveaway you will probably want to use a plugin or piece of software the specializes in giveaways. The most popular one by far is <a href="https://kingsumo.com/apps/giveaways/">KingSumo</a>.</p>
+			<p>It&rsquo;s a WordPress plugin that allows you to easily get a viral giveaway setup.</p>
+			<h2>Today&rsquo;s Tasks</h2>
+			<ul>
+				<li>Think about prizes that would fit your audience and are enticing enough that they&rsquo;ll want to enter</li>
+			</ul>
 			<div class="callout">
 				<p>Did you end up here because someone was kind enough to link this page? That&rsquo;s cool, but you&rsquo;ll want to sign up yourself so you can experience all 7 days of this list-building challenge.</p>
 				<p><a data-open="emailModal" class="button expanded">Start Your Free 7-Day Challenge</a></p>
